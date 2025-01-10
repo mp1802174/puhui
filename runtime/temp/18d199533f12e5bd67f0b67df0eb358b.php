87aa9a4263bd8d25b05bca7b7bc9c38dd9d7c039
mp1802174 <mp1802174@smail.nju.edu.cn>
2025-01-10 21:00:19 +0800

    正确显示市支网员四个层级了

--- runtime/temp/18d199533f12e5bd67f0b67df0eb358b.php
+++ runtime/temp/18d199533f12e5bd67f0b67df0eb358b.php
@@ -1,4 +1,4 @@
-<?php /*a:1:{s:59:"I:\phpstudy_pro\WWW\puhui\app\view\hierarchy\hierarchy.html";i:1736510590;}*/ ?>
+<?php /*a:1:{s:59:"I:\phpstudy_pro\WWW\puhui\app\view\hierarchy\hierarchy.html";i:1736513869;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,8 +21,8 @@
             <ul>
                 <?php foreach($data as $item): ?>
                 <li>
-                    <a href="?level=<?php echo htmlentities($next_level); ?>&parent_name=<?php echo htmlentities($item['name']); ?>">
-                        <?php echo htmlentities($item['name']); ?> - 余额: <?php echo htmlentities($item['balance']); ?>
+                    <a href="?level=<?php echo htmlentities($next_level); ?>&<?php if(isset($item['id'])): ?>parent_id=<?php echo htmlentities($item['id']); else: ?>parent_name=<?php echo htmlentities($item['name']); ?><?php endif; ?>" target="_blank">
+                        <?php echo htmlentities($item['name']); ?> - 余额: <?php if(isset($item['balance'])): ?><?php echo htmlentities($item['balance']); else: ?><?php echo htmlentities($item['total_balance']); ?><?php endif; ?>
                     </a>
                 </li>
                 <?php endforeach; ?>
