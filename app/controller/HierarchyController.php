9a83063573420a10d2ec088437b1787197305932
mp1802174 <mp1802174@smail.nju.edu.cn>
2025-02-08 14:06:12 +0800

    清空employ1

--- app/controller/HierarchyController.php
+++ app/controller/HierarchyController.php
@@ -284,7 +284,7 @@ class HierarchyController
         ]);
     }
 
-    private function getEmployeeData($accountingId, $date, $employeeName)
+    private function getEmployeeData($accountingId, $date)
     {
         
     }
